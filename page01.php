b44607daab4239877bf3859a43ccdd68a180ee9a
MarcosN00 <99758158+MarcosN00@users.noreply.github.com>
2022-04-06 16:40:35 -0300

    Create page01.php
    aula02 php c/ db

---
+++ page01.php
@@ -0,0 +1,20 @@
+<?php
+
+$carro = "fusca";
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula02</title>
+</head>
+<body>
+    <?php echo $carro ?>
+
+    <img src="https://picsum.photos/200/300" alt="Imagem aleatória">
+
+</body>
+</html>
