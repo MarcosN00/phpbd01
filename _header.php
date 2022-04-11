a5690c14d21fd45dfee7fdcc69e70d77962dd9ea
MarcosN00 <99758158+MarcosN00@users.noreply.github.com>
2022-04-11 16:43:01 -0300

    aula04
    php com banco de dados

--- _header.php
+++ _header.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <title>Vitugo</title>
+    <link rel="stylesheet" href="css/stylecss.css">
+    <title>Vitugo - <?php echo $title ?></title>
 </head>
 
 <body>
@@ -27,11 +28,11 @@
 
             <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney"></i><span>Início</span></a>
 
-            <a href="/contatos" title="Faça contato"><i class="fa-solid fa-comment-dots"></i><span>Contatos</span></a>
+            <a href="/_contacts.php" title="Faça contato"><i class="fa-solid fa-comment-dots"></i><span>Contatos</span></a>
 
-            <a href="/sobre" title="Sobre..."><i class="fa-solid fa-circle-info"></i><span>Sobre</span></a>
+            <a href="/about.php" title="Sobre..."><i class="fa-solid fa-circle-info"></i><span>Sobre</span></a>
 
-            <a href="/usuario" title="Usuário"><i class="fa-solid fa-user"></i><span>Usuário</span></a>
+            <a href="/user.php" title="Usuário"><i class="fa-solid fa-user"></i><span>Usuário</span></a>
 
         </nav>
 
